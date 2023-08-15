7c7aed72d58d7b27ed82f035a8a20a81eaa58f7a
Zohreh Rezazadeh <rezazadeh.zr@gmail.com>
2023-08-15 12:56:17 +0330

    about identifiers

---
+++ identifiers.py
@@ -0,0 +1,15 @@
+#variables-Identifier
+
+#pi=3.14   identifier= pi(اسم هاي متغيرها)  variable=3.14
+
+#identifires are case sensitive a=5, A=10
+
+#english letters, numbers, _        Pitza_area2
+
+# numbers can not be the first of identifiers -> 2aa(false)
+
+#python keywords (33)
+
+
+
+
