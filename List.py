8db7bd0c7cb23ea171b5e52ab4f2fc40e7200856
Zohreh Rezazadeh <rezazadeh.zr@gmail.com>
2023-08-16 15:47:49 +0330

    expression,statement,assignment, List

---
+++ List.py
@@ -0,0 +1,16 @@
+#List
+
+
+my_list=['h',7,6]
+my_list[0]
+print(my_list)
+print(my_list[0])
+
+my_list[0]=12
+print(my_list[0])
+
+print(my_list[-1])
+
+print(my_list[-2])
+
+print(type(my_list))
---
+++ Statement-Expression-Assignment.py
@@ -0,0 +1,23 @@
+#equal Statement
+#identifier=variable اين دو تا را بهم وصل مي کند
+
+a=5
+a=a+1
+print(a)
+
+#Expression
+#هر چيزي که يک مقداري را برمي گرداند
+#4    4+5->9   "Hello"  True
+
+#Statement
+#مقدار برنمي گردانند
+#a=3+1
+#print("hello")
+#if  a>3:
+
+
+#Assighnment
+#مقداردهي کردن
+#a=6
+#a=a+2
+#varname=expression
