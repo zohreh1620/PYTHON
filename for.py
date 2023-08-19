6f75aa4247d7eaadad39aa829dd1970ac6b2b8d1
zohreh1620 <rezazadeh.zr@gmail.com>
2023-08-19 13:52:02 +0330

    for

---
+++ for.py
@@ -0,0 +1,39 @@
+#for
+
+#for iteration_var in sequence:
+#    statement(s)
+#    ...
+#    ...
+
+#statement
+
+
+books=[11,2,5,8]
+
+for i in books:
+    print(i)
+
+print("finished")
+
+
+
+
+
+step=5
+while step>0:
+    print("HELLO1")
+    step-=1
+print("Finished1")
+
+
+
+for x in [1,2,3,4,5]:
+    print("HELLO2")
+
+print("Finished2")
+
+
+for x in range(0,5):
+    print("FHELLO3")
+
+print("Finished3") 
