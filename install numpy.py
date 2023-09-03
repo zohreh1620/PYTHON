f1058bd1164c5847cd61225b404e010bee17aa7d
zohreh1620 <rezazadeh.zr@gmail.com>
2023-09-03 09:57:22 +0330

    2023-09-03

---
+++ install numpy.py
@@ -0,0 +1,7 @@
+# install numpy
+# aval bayad pip nasb bashad ke agar az noskheye asli estefade konim default nasb hast
+# baad dar cmd minevisim  pip help    ke agar javab dad yani pip nasb ast
+#baraye nasb numpy dar cmd windows dasture zir ra minevisim
+
+# cmd windows
+pip install numpy
---
+++ py version.py
@@ -0,0 +1,3 @@
+# get python version in windows
+
+py --version
